b56d91acf1cb938621e7f72761248a173e33951b
Sergio <sggcoding@gmail.com>
2022-02-02 04:06:11 +0000

    pre ~ 02.02.22 @ 04:05AM | [Creating TypeWriter] - creating a typewriter from scratch with JavaScript

---
+++ assets/js/typewriter.js
@@ -0,0 +1,53 @@
+const textDisplay = document.getElementById('typewriter')
+const phrases = ['I am a Frontend Developer.', 'I help build the web.', 'I help create digital art.']
+let i = 0
+let j = 0 
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * 80
+  const normalSpeed = Math.random() * 350
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
+
+
+
+
+
+
