3d25b70664f7eccd42d797f2e9f1cd418627f95f
Sergio <sggcoding@gmail.com>
2022-02-03 12:00:38 +0000

    pre ~ 03.02.22 @ 12.00PM | [Correcting some Padding Issues on Mobile Devices] - for responsiveness

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -1,5 +1,5 @@
 const textDisplay = document.getElementById('typewriter')
-const phrases = ["I'm a Frontend Dev.", "I help build the web.", "I help create digital art."]
+const phrases = ["I'm a Frontend Dev.", "I help build the web.", "I create digital art."]
 let i = 0
 let j = 0 
 let currentPhrase = []
